7c79a74a198468f027764718bfe0f798e805fa70
Robert Collins <robert.collins@cognite.com>
2020-05-08 20:50:37 +1200

    Migrate benchmarks to Criterion

--- benches/is_enabled.rs
+++ benches/is_enabled.rs
@@ -7,13 +7,11 @@
 // higher in-loop costs as it has to record iterations, so this bench will still
 // be useful.
 
-#![feature(test)]
-extern crate test;
-
 use std::sync::Arc;
 use std::thread;
-use test::Bencher;
+use std::time::Duration;
 
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
 use maplit::hashmap;
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
@@ -22,16 +20,12 @@ use unleash_api_client::api::{Feature, Features, Strategy};
 use unleash_api_client::client;
 use unleash_api_client::context::Context;
 
-fn client(
-    count: usize,
-) -> Result<
-    client::Client<http_client::native::NativeClient>,
-    Box<dyn std::error::Error + Send + Sync + 'static>,
-> {
+fn client(count: usize) -> client::Client<http_client::native::NativeClient> {
     let client = client::ClientBuilder::default()
-        .into_client::<http_client::native::NativeClient>("notused", "app", "test", None)?;
+        .into_client::<http_client::native::NativeClient>("notused", "app", "test", None)
+        .unwrap();
     let mut features = vec![];
-    for i in 1..count + 1 {
+    for i in 0..count {
         let name = format!("flexible{}", i);
         features.push(Feature {
             description: name.clone(),
@@ -51,112 +45,125 @@ fn client(
         features,
     };
     client.memoize(f.features).unwrap();
-    Ok(client)
+    client
 }
 
-#[bench]
-fn parallel_same(
-    b: &mut Bencher,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+fn batch(c: &mut Criterion) {
     let _ = simple_logger::init_with_level(log::Level::Warn);
     let cpus = num_cpus::get();
-    let client = Arc::new(client(cpus)?);
-    let iterations = 10_000;
+    let client = Arc::new(client(cpus));
+    let iterations = 50_000;
     println!(
         "Benchmarking across {} threads with {} iterations per thread",
         cpus, iterations
     );
-    b.iter(|| {
-        let mut threads = vec![];
-        for _cpu in 0..cpus {
-            let thread_client = client.clone();
-            let feature = format!("flexible1");
-            let handle = thread::spawn(move || {
-                let context = Context {
-                    user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
-                    ..Default::default()
-                };
-                for _ in 0..iterations {
-                    thread_client.is_enabled(&feature, Some(&context), false);
-                }
-            });
-            threads.push(handle);
-        }
-        for thread in threads {
-            thread.join().unwrap();
-        }
+    let mut group = c.benchmark_group("batch");
+    group
+        .throughput(Throughput::Elements(iterations as u64))
+        .warm_up_time(Duration::from_secs(15))
+        .measurement_time(Duration::from_secs(30));
+    group.bench_function("single thread", |b| {
+        b.iter(|| {
+            // Context creation is in here to make this comparable to parallel_same above.
+            let context = Context {
+                user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
+                ..Default::default()
+            };
+            for _ in 0..iterations {
+                client.is_enabled("flexible0", Some(&context), false);
+            }
+        })
     });
-    Ok(())
+    group
+        .throughput(Throughput::Elements(iterations * cpus as u64))
+        .sample_size(10);
+    group.bench_function("parallel same-feature", |b| {
+        b.iter(|| {
+            let mut threads = vec![];
+            for _cpu in 0..cpus {
+                let thread_client = client.clone();
+                let feature = format!("flexible0");
+                let handle = thread::spawn(move || {
+                    let context = Context {
+                        user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
+                        ..Default::default()
+                    };
+                    for _ in 0..iterations {
+                        thread_client.is_enabled(&feature, Some(&context), false);
+                    }
+                });
+                threads.push(handle);
+            }
+            for thread in threads {
+                thread.join().unwrap();
+            }
+        })
+    });
+    group.bench_function("parallel distinct-features", |b| {
+        b.iter(|| {
+            let mut threads = vec![];
+            for cpu in 0..cpus {
+                let thread_client = client.clone();
+                let feature = format!("flexible{}", cpu);
+                // println!("{}", feature);
+                let handle = thread::spawn(move || {
+                    let context = Context {
+                        user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
+                        ..Default::default()
+                    };
+                    for _ in 0..iterations {
+                        thread_client.is_enabled(&feature, Some(&context), false);
+                        // thread::sleep(Duration::from_nanos(1));
+                    }
+                });
+                threads.push(handle);
+            }
+            for thread in threads {
+                thread.join().unwrap();
+            }
+        })
+    });
+    group.bench_function("parallel unknown-features", |b| {
+        b.iter(|| {
+            let mut threads = vec![];
+            for cpu in 0..cpus {
+                let thread_client = client.clone();
+                let feature = format!("unknown{}", cpu);
+                let handle = thread::spawn(move || {
+                    let context = Context {
+                        user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
+                        ..Default::default()
+                    };
+                    for _ in 0..iterations {
+                        thread_client.is_enabled(&feature, Some(&context), false);
+                    }
+                });
+                threads.push(handle);
+            }
+            for thread in threads {
+                thread.join().unwrap();
+            }
+        })
+    });
+    group.finish();
 }
 
-#[bench]
-fn parallel_sharded(
-    b: &mut Bencher,
-) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
+fn single_call(c: &mut Criterion) {
     let _ = simple_logger::init_with_level(log::Level::Warn);
-    let cpus = num_cpus::get();
-    let client = Arc::new(client(cpus)?);
-    let iterations = 10_000;
-    println!(
-        "Benchmarking across {} threads with {} iterations per thread",
-        cpus, iterations
-    );
-    b.iter(|| {
-        let mut threads = vec![];
-        for cpu in 0..cpus {
-            let thread_client = client.clone();
-            let feature = format!("flexible{}", cpu);
-            let handle = thread::spawn(move || {
-                let context = Context {
-                    user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
-                    ..Default::default()
-                };
-                for _ in 0..iterations {
-                    thread_client.is_enabled(&feature, Some(&context), false);
-                }
-            });
-            threads.push(handle);
-        }
-        for thread in threads {
-            thread.join().unwrap();
-        }
-    });
-    Ok(())
-}
-
-#[bench]
-fn single_loop(b: &mut Bencher) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let _ = simple_logger::init_with_level(log::Level::Warn);
-    let cpus = num_cpus::get();
-    let client = client(cpus)?;
-    let iterations = 10_000;
-    println!(
-        "Benchmarking across 1 threads with {} iterations per thread, {} defined features",
-        iterations, cpus
-    );
-    b.iter(|| {
-        // Context creation is in here to make this comparable to parallel_same above.
-        let context = Context {
-            user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
-            ..Default::default()
-        };
-        for _ in 0..iterations {
-            client.is_enabled("flexible1", Some(&context), false);
-        }
-    });
-    Ok(())
-}
-
-#[bench]
-fn single_call(b: &mut Bencher) -> Result<(), Box<dyn std::error::Error + Send + Sync + 'static>> {
-    let _ = simple_logger::init_with_level(log::Level::Warn);
-    let client = client(1)?;
+    let client = client(1);
     let context = Context {
         user_id: Some(thread_rng().sample_iter(&Alphanumeric).take(30).collect()),
         ..Default::default()
     };
-    b.iter(|| {
-        client.is_enabled("flexible1", Some(&context), false);
+    let mut group = c.benchmark_group("single_call");
+    group.throughput(Throughput::Elements(1));
+    group.bench_function("single_call", |b| {
+        b.iter(|| {
+            client.is_enabled("flexible0", Some(&context), false);
+        })
     });
-    Ok(())
+    group.finish();
 }
+
+criterion_group!(benches, single_call, batch);
+criterion_main!(benches);
