dcff01784573d2e516c3b98e1afebc688763ba35
Ikechukwu Eze <92305839+ikezedev@users.noreply.github.com>
2025-07-18 11:27:28 +0200

    default current_time to now

--- src/context.rs
+++ src/context.rs
@@ -27,7 +27,7 @@ impl<'de> de::Deserialize<'de> for IPAddress {
     }
 }
 
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct Context {
     #[serde(rename = "userId")]
@@ -42,6 +42,21 @@ pub struct Context {
     pub app_name: String,
     #[serde(default)]
     pub environment: String,
-    #[serde(rename = "currentTime")]
-    pub current_time: Option<DateTime<Utc>>,
+    /// Defaults to the current time.
+    #[serde(rename = "currentTime", default = "Utc::now")]
+    pub current_time: DateTime<Utc>,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        Context {
+            user_id: Default::default(),
+            session_id: Default::default(),
+            remote_address: Default::default(),
+            properties: Default::default(),
+            app_name: Default::default(),
+            environment: Default::default(),
+            current_time: Utc::now(),
+        }
+    }
 }
